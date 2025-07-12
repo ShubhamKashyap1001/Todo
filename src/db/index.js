b6ff1ef72d9331cb812fe09c25b40753c1a80df9
ShubhamKashyap1001 <shubh9142996613@gmail.com>
2025-07-12 14:39:22 +0530

    Implement database connection function

--- src/db/index.js
+++ src/db/index.js
@@ -1,2 +1,14 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js"
+import { ApiError } from "../utils/ApiError.js";
 
+const connectDB = async ()=> {
+    try {
+        const connectInstances = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`DB CONNECTION SUCCESSFULLY ${connectInstances.connection.host}`);
+    }catch (error) {
+        throw new ApiError(`DB CONNECTION FAILED !!  ${error}`)
+    }
+}
+
+export default connectDB
