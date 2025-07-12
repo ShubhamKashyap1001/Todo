42117862a354d2340494642f9323a738ac76f910
ShubhamKashyap1001 <shubh9142996613@gmail.com>
2025-07-12 13:05:55 +0530

    Implement task management functions: add, remove, and update tasks

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,82 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
+import { Task } from "../models/task.model";
+
+const addTask = asyncHandler(async() => {
+    const {task} = req.body;
+    if(!task){
+        throw new ApiError(400,"Task Doesn't add successfully")
+    }
+
+    const newTask = await Task.create({task});
+    console.log("Task added successfully");
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            "Task add successfully",
+            newTask,
+        )
+    )
+    
+})
+
+const removeTask = asyncHandler(async() => {
+    const { id } = req.params;
+    
+    const findTask = await Task.findById(id)
+    if(!findTask){
+        throw new ApiError(4001,"Task is not present")
+    }
+
+    await Task.findByIdAndDelete(id);
+    console.log("Task remove successfully");
+    
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            "Task removed successfuly",
+        )
+    )
+})
+
+const updateTask = asyncHandler(async(req,res) => {
+
+    const {id} = req.params;
+    const {title,completed} = req.body;
+
+    if(!title || ! completed){
+        throw new ApiError(400 ," All Fields are required")
+    }
+
+    const update = await Task.findByIdAndUpdate(
+        id,
+        {title,completed},
+        {new : true}
+    );
+
+    console.log("Task update successfully");
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            "Task update successfully",
+            update,
+        )
+    )
+    
+})
+
+export {
+    addTask,
+    removeTask,
+    updateTask,
+}
